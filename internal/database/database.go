28e525bcfc6b45b851b3ebeae4d77a1664705ff2
Malov Tim <t.malov2014@gmail.com>
2026-03-10 23:34:07 +0300

    databse connection added

---
+++ internal/database/database.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewDB() (*pgxpool.Pool, error) {
+
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	port := os.Getenv("POSTGRES_PORT")
+	host := os.Getenv("POSTGRES_HOST")
+
+	connString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		user,
+		password,
+		host,
+		port,
+		dbName)
+
+	pool, err := pgxpool.New(context.Background(), connString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = pool.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
